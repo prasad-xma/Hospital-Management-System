a3e7bb94c1fbc29bf2913aa8fa60263583d220fd
Prasad Manamperi <prasadmanamperi130@gmail.com>
2025-10-12 23:43:59 +0530

    Add Tailwind CSS and plugin dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,41 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
+      <div className="flex gap-4">
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="w-20 h-20" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="w-20 h-20" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+
+      <h1 className="text-3xl font-bold mt-4">Vite + React</h1>
+
+      <div className="card mt-4 p-4 border rounded">
+        <button
+          onClick={() => setCount((count) => count + 1)}
+          className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+        >
           count is {count}
         </button>
-        <p>
+        <p className="mt-2">
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+
+      <p className="text-blue-800 font-bold mt-4">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
 export default App
