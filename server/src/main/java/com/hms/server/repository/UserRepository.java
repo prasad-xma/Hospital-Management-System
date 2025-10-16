69375992d2f415ebb23190c248a71803582e6dae
Prasad Manamperi <prasadmanamperi130@gmail.com>
2025-10-17 02:25:20 +0530

    update user repository fetch user by role

--- server/src/main/java/com/hms/server/repository/UserRepository.java
+++ server/src/main/java/com/hms/server/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.hms.server.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -20,4 +21,6 @@ public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByUsernameAndIsActiveTrue(String username);
     
     Optional<User> findByEmailAndIsActiveTrue(String email);
+
+    List<User> findByRolesContaining(User.Role role);
 }
